3cc6bf27c514fda22522b49aac4e588896309e2e
loc-dev <nguyentheloc310101@gmail.com>
2024-07-13 15:09:52 +0700

    fix: antd CSS deplay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { TableCustom } from '@/components/table';
+import { Button } from 'antd';
 
 export default function Home() {
   return (
     <>
       <TableCustom />
+      <Button type='primary'>test</Button>
     </>
   );
 }
